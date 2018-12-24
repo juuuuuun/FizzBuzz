20cba230163859ac837861af6ebec565bc27dcfc
Jun Oh <kyungjunjoh@gmail.com>
2018-12-24 00:28:15 -0800

    Wrote the main function for FizzBuzz

--- FizzBuzz/main.c
+++ FizzBuzz/main.c
@@ -9,7 +9,26 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    for(int i=1; i<101; i++) {
+        // If the number is multiple of 3, write Fizz
+        // This condition comes before checking multiple of 5 since we want FizzBuzz
+        if(i % 3 == 0) {
+            printf("Fizz");
+        }
+        // If the number is multiple of 5, write Buzz
+        // Buzz will be appended to Fizz if the number is both multiple of 3 and 5
+        if(i % 5 == 0) {
+            printf("Buzz");
+        }
+        // If the number is neither multiple of 3 nor 5, write the number
+        if(i % 3 != 0 && i % 5 != 0) {
+            printf("%d", i);
+        }
+        // After each number or Fizz/Buzz, add a new line
+        printf("\n");
+    }
+    
     return 0;
+    
 }
